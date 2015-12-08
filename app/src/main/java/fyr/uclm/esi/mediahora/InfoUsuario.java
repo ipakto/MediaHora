11ed0047a0ca1aa6e6762cf0e4fb4be298743089
Paco <pco.ruiz94@gmail.com>
2015-12-08 12:35:44 +0100

    Ventana info usuario

--- app/src/main/java/fyr/uclm/esi/mediahora/InfoUsuario.java
+++ app/src/main/java/fyr/uclm/esi/mediahora/InfoUsuario.java
@@ -1,11 +1,13 @@
 package fyr.uclm.esi.mediahora;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.preference.PreferenceManager;
 import android.widget.TextView;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Rosana on 15/11/2015.
@@ -13,20 +15,29 @@ import butterknife.Bind;
 public class InfoUsuario extends Activity
 {
     // Layout components
+    @Bind(R.id.txtNombre) TextView txtNombre;
+    @Bind(R.id.txtCorreo) TextView txtCorreo;
+    @Bind(R.id.tAltura) TextView tAltura;
+    @Bind(R.id.tZancada) TextView tZancada;
+    @Bind(R.id.tPeso) TextView tPeso;
     @Bind(R.id.lblAltura) TextView lblAltura;
-    @Bind(R.id.lblCorreo)TextView lblCorreo;
     @Bind(R.id.lblPeso) TextView lblPeso;
     @Bind(R.id.lblZancada) TextView lblZancada;
-    @Bind(R.id.txtAltura) TextView txtAltura;
-    @Bind(R.id.txtCorreo) TextView txtCorreo;
-    @Bind(R.id.txtPeso) TextView txtPeso;
-    @Bind(R.id.txtZancada) TextView txtZancada;
-    @Bind(R.id.txtNombre) TextView txtNombre;
-    @Bind(R.id.imgUsuario)ImageView imgUsuario;
+    @Bind(R.id.fotoPerfil) de.hdodenhof.circleimageview.CircleImageView fotoPerf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_usuario);
+        ButterKnife.bind(this);
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(this);
+        txtNombre.setText(prefs.getString("pNombre", getStr(R.string.desc_nombre)));
+        txtCorreo.setText(prefs.getString("pCorreo", getStr(R.string.desc_correo)));
+        tAltura.setText(prefs.getInt("pAltura", 0)+" cm.");
+        tZancada.setText(prefs.getInt("pDistanciaP", 0)+" cm.");
+        tPeso.setText(prefs.getInt("pPeso", 0) + " kg.");
+    }
+    private String getStr(int id){
+        return getResources().getString(id);
     }
 }
